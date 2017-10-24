afd03067f92352194606258608b8609707bcfabb
Destoni Baldwin <tonibwin@hotmail.com>
2017-10-23 20:11:01 -0600

    PageModel.cs initial commit. Incomplete

---
+++ dev/PageModel.cs
@@ -0,0 +1,104 @@
+﻿/* PageModel.cs  
+ * Written By Destoni Baldwin
+ * Wrriten on 10/20/2017
+ * Purpose: Retrieves information form both TextModel.cs and DocumentModel.cs to hold text.
+ * Controls when text is inserted or removed and should update cursor position when text is changed.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Collections;
+namespace Won.Model {
+
+    public class PageModel { }
+
+    public class Page : INotifyPropertyChanged{
+
+        //Instance Variables
+        private int cursor;
+        public ObservableCollection<Text> text = new ObservableCollection<Text>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string property){
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        //Default Constructor
+        public Page() { }
+
+        //A constructor that creates a page object giving it the data
+        //for a page number and the text that belongs on the page.
+        //Pre-Condition: Input of pos variable and text variable.
+        //Post-Condition: Creates a page object
+        public Page(int cursor, ObservableCollection<Text> text)
+        {
+            this.cursor = cursor;
+            this.text = text;
+        }
+
+        //Allows to input character text into the list that holds
+        //text on the page.
+        //Pre-Conditions: Takes in characters to input as text
+        //Post-Conditions: Increments cursor position and character 
+        //input is added into the ObservableCollection 'text'.
+        public void inputText(char input){
+            Cursor++;
+            text.Insert(Cursor, input);
+            //Error: Unclear why I can't use Insert() or Add() for 
+            //the Observable Collection
+        }
+
+        //Removes text from ObservableCollection
+        //Pre-Condition: None
+        //Post-Condition: Removes text from cursor position. Decrements 
+        //Cursor.
+        public void delText(){
+            if (text.Count() != 0){
+                text.RemoveAt(Cursor);
+                Cursor--;
+            }
+        }
+
+        //Pre-Conditions: Takes in boolean parameter called type.
+        //Post-Conditions: If true should pass the ObservableCollection<Text> 
+        //to the document class. If false should take in txt file from 
+        //document class and place into ObservableCollection<Text>
+        public void PassText(bool type){
+
+        }
+
+        //Cursor Position Getter and Setter
+        public int Cursor{
+            get{ return cursor; }
+            set{
+                if (cursor != value){
+                    cursor = value;
+                    RaisePropertyChanged("Cursor");
+                    RaisePropertyChanged("Text");
+                }
+            }
+        }
+
+        //ObservableCollection<Text> Text Getter and Setter
+        public ObservableCollection<Text> Text{ 
+            get { return text; }
+            set{
+                text = value;
+                RaisePropertyChanged("Cursor");
+                RaisePropertyChanged("Text");
+            }
+        }
+    }
+}
+
+
+
---
+++ dev/TextModel.cs
@@ -0,0 +1,87 @@
+﻿/* TextModel.cs
+ * Written by Jordan Leibman
+ * Written on 10/19/2017
+ * Purpose: Controls the formatting of individual text characters for bold, italics, and underline
+ * TODO: unit and integration testing with PageModel.cs
+ * Implications: Text is stored in a list on PageModel.cs and used occasionally for saving through the DocumentModel.cs
+*/
+using System;
+using System.ComponentModel;
+
+namespace Won.Model {
+   public class TextModel { }
+   public class Text : INotifyPropertyChanged {
+      private char ch;
+      private bool bold;
+      private bool italics;
+      private bool underline;
+      
+      /*Ch
+       * The character stored in the Text object
+       * Returns a non formatted character
+       */
+      public char Ch {
+         get { return ch; }
+         set {
+            if (ch != value) ch = value;
+            RaisePropertyChanged("Ch");
+            RaisePropertyChanged("Text");
+         }
+      }
+
+      /*Bold
+       * The flag if Text is bold
+       * returns if text is bolded
+       */
+      public bool Bold {
+         get { return bold; }
+         set {
+            if (bold != value) bold = value;
+            RaisePropertyChanged("Bold");
+            RaisePropertyChanged("Text");
+         }
+      }
+
+      /*Italics
+       * The flag if Text is italics
+       * returns if text is italicized
+       */
+      public bool Italics {
+         get { return italics; }
+         set {
+            if (italics != value) italics = value;
+            RaisePropertyChanged("Italics");
+            RaisePropertyChanged("Text");
+         }
+      }
+
+      /*Underline
+       * The flag if Text is underline
+       * returns if text is underlined
+       */
+      public bool Underline {
+         get { return underline; }
+         set {
+            if (underline != value) underline = value;
+            RaisePropertyChanged("Underline");
+            RaisePropertyChanged("Text");
+         }
+      }
+
+      /* ToString
+       * Written by Jordan Leibman
+       * returns the Text object as a string with the following format
+       * Ch|Bold|Italics|Underline
+       * Used as a standard format for the saving and loading of files
+       */
+      public string ToString() {
+         return ch.ToString() + "|" + bold.ToString() + "|" + italics.ToString() + "|" + underline.ToString();
+      }
+      //Event and property handlers
+      public event PropertyChangedEventHandler PropertyChanged;
+
+      private void RaisePropertyChanged(string property) {
+         if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(property));
+      }
+   }
+}
