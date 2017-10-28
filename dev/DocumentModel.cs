a66b1fa04516d543f30ba483f4e018d3078bd14b
Jordan Leibman <jordan.leibman@gmail.com>
2017-10-28 16:36:05 -0600

    Post-merge cleanup

--- dev/DocumentModel.cs
+++ dev/DocumentModel.cs
@@ -1,52 +1,48 @@
-﻿using System.ComponentModel;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
-using System.Collections.ObjectModel;
-using System;
+using System.Text;
 
-namespace Won.Model{
-   public class DocumentModel { }
-   public class Document : INotifyPropertyChanged {
-      private string fileName;
-      public ObservableCollection<Page> pages;
 
-      public string FileName {
-         get { return fileName;  }
-         set {
-            //filename check before setting
-            //update filename
-            //cannot contain @[^\/:*?"<>|]+
-            Regex prevent = new Regex("[^\\/:*?\"<>\\|]");
-            if (!prevent.IsMatch(value)) {
-               fileName = value;
-               RaisePropertyChanged("FileName");
-            }
-            else Console.WriteLine("FileName invalid, Cannot contain \\/:*?\"<>\\| ");
-         }
-      }
+/*
+ Author: Michael Curtis
+ Date: 10/20/17
+*/
+namespace Won.Model
+{
+    class DocumentModel
+    { }
+    class Document { 
+        private String fileName;
+        private ObservableCollection<PageModel> pages;
+        private event EventHandler PropertyChangedHandler;
 
-      public bool Save() {
-         //save to a text file using the ToString Method from Text
-            using (System.IO.StreamWriter file = new System.IO.StreamWriter(fileName)) ;
+        public Document()
+        {
+            fileName = "";
+            pages = null;
+        }
+
+        public Document(String createFileName, ObservableCollection<PageModel> pages)
+        {
+            this.fileName = createFileName;
+            this.pages = pages;
+        }
+
+        public void save()
+        {
             
-            return false;
-      } // saves file by using text.toString to a text file into a specified format
-      public bool SaveAs(string file) {
-         FileName = file; //attemps to change filename, if successful saves
-         if (FileName == file) return Save();
-         else Console.WriteLine("SaveAs Failed");
-         return false;
+        }
 
-      } 
-      public bool Load(string file) {
-         return false;
-      } //loads opposite of the specified format and adds text object to page's Text collection
+        public void saveAs(string newFileName)
+        {
+            this.fileName = newFileName;
+            return this.fileName;
+        }
 
-      public event PropertyChangedEventHandler PropertyChanged;
+        public void load(string fileName)
+        {
 
-      private void RaisePropertyChanged(string property) {
-         if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(property));
-      }
-   }
+        }
+    }
 }
