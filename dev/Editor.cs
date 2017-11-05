82615b08a87b7012c9af392c496764433ed0aa2d
Destoni Baldwin <tonibwin@hotmail.com>
2017-11-05 07:44:37 -0700

    Event Handlers and Methods for Font Size and Font Family.

--- dev/Editor.cs
+++ dev/Editor.cs
@@ -27,13 +27,61 @@ namespace Won.Editor{
       //Constructor Class
       //Fill this with the data values you need initialized but leave out any component initialization until we bind to the GUI or your program won't run
       public Editor(){
-         
-      }
+            InitializeComponent();
+            //Uses SystemFontFamilies class to retrieve different styles
+            cmbFontFamily.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+            //List of all possible sizes for font
+            cmbFontSize.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
 
-      //Save/Load Features
+        }
 
-      //Styles(Bold, Italics, Underline)
+        //Save/Load Features
 
-      //Fonts(Style and Size)
-   }
+        //Styles(Bold, Italics, Underline)
+
+        //Fonts(Style and Size)
+
+        /* Written By Destoni Baldwin
+         * 11/3/17
+         * Editor_SelectionChanged is to update the editor whenever the cursor moves or when 
+         * A selection has been changed. It should immediately update the editor's view whenever
+         * the cursor moves or a selection is changed
+         * Pre-Conditions: Takes in an object aka a selection or cursor move that is found by 
+         * an event. Parameters are event handlers.
+         * Post-Conditions: Updates the FontFamily and Font size of selected text.
+         */
+        private void UpdateFontSizeAndFamily(object sender, RoutedEventArgs e)
+        {
+            //Font Family
+            object temp = rtbEditor.Selection.GetPropertyValue(Inline.FontFamilyProperty);
+            cmbFontFamily.SelectedItem = temp;
+
+            //Font Size
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontSizeProperty);
+            cmbFontSize.Text = temp.ToString();
+        }
+
+        /* Written By Destoni Baldwin
+         * Purpose of function is to be able to select a font family from the toolbar.
+         * Pre-Conditions: Parameters are eventhandlers in which takes in a selected change 
+         * of Font Family.
+         * Post-Conditions: Updates Font Family selection in toolbar.
+         */
+        private void selectFontFamily(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmbFontFamily.SelectedItem != null)
+                rtbEditor.Selection.ApplyPropertyValue(Inline.FontFamilyProperty, cmbFontFamily.SelectedItem);
+        }
+
+        /* Written By Destoni Baldwin
+         * Purpose of function is to be able to select a font size from the toolbar.
+         * Pre-Conditions: Parameters are eventhandlers in which takes in a selected change 
+         * of Font size.
+         * Post-Conditions: Updates Font size selection in toolbar.
+         */
+        private void selectFontSize(object sender, TextChangedEventArgs e)
+        {
+            rtbEditor.Selection.ApplyPropertyValue(Inline.FontSizeProperty, cmbFontSize.Text);
+        }
+    }
 }
